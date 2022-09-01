436e167df6d84a7c4aee9a4cce6c8c15bdb1f277
ayumatsunaga <98738745+ayumatsunaga@users.noreply.github.com>
2022-09-01 11:02:07 +0900

    Update charts_02.py
    相対パスでディレクトリ取得するように変更

--- charts_02.py
+++ charts_02.py
@@ -8,6 +8,13 @@ Created on Wed Aug 24 13:27:36 2022
 import streamlit as st
 import pandas as pd
 import datetime
+import os
+import glob
+
+iDir = iDir = os.getcwd()#このプログラムと同じディレクトリ
+filename = glob.glob('ダッシュボードデータ.xlsx')
+Filename = "".join(filename)#上のままではリストなので文字列に直す
+FILE = iDir+ "\\" + Filename#'C:\\Users\\pccnf\\Desktop\\making_dashboard\\ダッシュボードデータ.xlsx'
 
 st.set_page_config(layout="wide")#layout=”wide”としないとグラフが中心よって両サイドに不必要な空白が生まれる
 
@@ -16,7 +23,8 @@ st.header('経産省　鉱工業指数')
 
 left_column, right_column = st.columns(2)
 left_column.subheader('電子部品・デバイス')
-df = pd.read_excel(r'C:\Users\pccnf\Desktop\streamlit\ダッシュボードデータ.xlsx',sheet_name='電子部品デバイス',index_col='年月')
+df = pd.read_excel(FILE,sheet_name='電子部品デバイス',index_col='年月')
+#df = pd.read_excel(r'C:\Users\pccnf\Desktop\streamlit\ダッシュボードデータ.xlsx',sheet_name='電子部品デバイス',index_col='年月')
 df_ri = df.reset_index()
 df_ri['年月'].max().strftime('%Y/%m')
 df_re = df_ri['年月'].dt.strftime('%Y/%m')
@@ -45,7 +53,7 @@ left_column.line_chart(df, width=900, height=500, use_container_width=False)
 
 
 right_column.subheader('電気・情報通信機器')
-df = pd.read_excel(r'C:\Users\pccnf\Desktop\streamlit\ダッシュボードデータ.xlsx',sheet_name='電気情報通信機器',index_col='年月')
+df = pd.read_excel(FILE,sheet_name='電気情報通信機器',index_col='年月')
 
 df_ri = df.reset_index()
 df_ri['年月'].max().strftime('%Y/%m')
@@ -56,7 +64,7 @@ df_ri.insert(0, 'year-month', df_re)
 right_column.line_chart(df, width=900, height=500, use_container_width=False)
 
 left_column.subheader('在庫率')
-df = pd.read_excel(r'C:\Users\pccnf\Desktop\streamlit\ダッシュボードデータ.xlsx',sheet_name='在庫率',index_col='年月')
+df = pd.read_excel(FILE,sheet_name='在庫率',index_col='年月')
 
 df_ri = df.reset_index()
 df_ri['年月'].max().strftime('%Y/%m')
@@ -71,7 +79,7 @@ st.header('経産省　生産動態統計')
 
 left_column, right_column = st.columns(2)
 left_column.subheader('品目別国内生産高の推移')
-df = pd.read_excel(r'C:\Users\pccnf\Desktop\streamlit\ダッシュボードデータ.xlsx',sheet_name='生産動態項目別生産金額',index_col='年月')
+df = pd.read_excel(FILE,sheet_name='生産動態項目別生産金額',index_col='年月')
 
 df_ri = df.reset_index()
 df_ri['年月'].max().strftime('%Y/%m')
@@ -91,5 +99,4 @@ st.header('経産省　特定サービス産業動態統計')
 
 left_column, right_column = st.columns(2)
 left_column.subheader('情報サービス産業の売上高の推移（4~9月）')
-df = pd.read_excel(r'C:\Users\pccnf\Desktop\streamlit\ダッシュボードデータ.xlsx',sheet_name='特サビ統計',index_col='年')
-
+df = pd.read_excel(FILE,sheet_name='特サビ統計',index_col='年')
